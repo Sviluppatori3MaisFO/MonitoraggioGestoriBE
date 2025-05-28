dec4e14e0693f1242668e9144b93528d1b8f9b14
sviluppatori3 <sviluppatori3@mais.services>
2025-05-28 16:51:19 +0200

    web config e Program.cs

--- MonitoraggioGestoriBE/Program.cs
+++ MonitoraggioGestoriBE/Program.cs
@@ -3,20 +3,21 @@ using MonitoraggioGestoriBE.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ Aggiungi servizi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<UtilsService>();
+builder.Services.AddTransient<UtilsService>();
 
 
-// ✅ Configura il DbContext Oracle
+//  Configura il DbContext Oracle
 var connectionString = builder.Configuration.GetConnectionString("FlussiFinDB");
+/*
 builder.Services.AddDbContext<FlussiFinContext>(options =>
     options.UseLazyLoadingProxies()
-        .UseOracle(connectionString));
+        .UseOracle(connectionString));*/
+builder.Services.AddDbContext<FlussiFinContext>(opt =>
+    opt.UseOracle(connectionString, oracleOptionsAction => oracleOptionsAction.UseOracleSQLCompatibility(OracleSQLCompatibility.DatabaseVersion19)));
 
-// ✅ CONFIGURA CORS
 builder.Services.AddCors(o => o.AddPolicy("AllowFrontend", builderCors =>
 {
     builderCors.AllowAnyOrigin()
@@ -26,20 +27,19 @@ builder.Services.AddCors(o => o.AddPolicy("AllowFrontend", builderCors =>
 
 var app = builder.Build();
 
-// ✅ Configura la pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
-// ✅ ATTIVA CORS
 app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
-app.MapControllers(); // ✅ Mappa i controller
+app.MapControllers();
 
 app.Run();
